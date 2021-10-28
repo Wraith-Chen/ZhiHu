30d2cc2edb80685df78c239f779989061c5b3582
Wraith <1363380539@qq.com>
2021-10-28 22:25:19 +0800

    2021.10.28

---
+++ app/src/main/java/com/example/zhihu/ui/notifications/VipViewModel.java
@@ -0,0 +1,19 @@
+package com.example.zhihu.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class VipViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public VipViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
